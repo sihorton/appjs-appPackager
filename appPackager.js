9604b35099db1653ff008450f60cee8ba5f2b5c5
Simon Horton <sij.horton@gmail.com>
2012-11-01 18:36:01 +0100

    Implemented packaging of node_modules, for now I reused same packaging as for applications (this can be improved later on), created a dependancies.json file that lists all of the required modules for the application.

--- appPackager.js
+++ appPackager.js
@@ -6,7 +6,22 @@ if (process.argv.length < 3) {
 	console.log("node.exe appPackager <folder to pack or package to unpack>");
 	process.exit(1);
 }
-
+var appPackage = "";
+var modulesWaiting = 0;
+var modulesWritten = function() {
+	console.log("modules written");
+	fs.writeFile(appFolder+"/node_modules/dependancies.json",JSON.stringify(moduleDependancies, null,4),function(err) {
+		if (err) {
+			console.log(err);
+		} else {
+			console.log("node_modules/dependancies.json written");
+		}
+		packageApp();
+	});
+}
+var moduleDependancies = {};
+var appFolder = './';
+				
 var fs = require("fs");
 fs.stat(process.argv[2], function(err, stats) {
 	if (err) {
@@ -14,69 +29,24 @@ fs.stat(process.argv[2], function(err, stats) {
 		process.exit(1);
 	}
 	if (stats.isDirectory()) {
-		var appFolder = process.argv[2];
-		
-		//scan node modules
-		console.log("scanning node_modules");
-		var moduleDependancies = {};
-		fs.readdir("./"+appFolder+"/node_modules", function(err, list) {
-			if (err) {console.log(err);throw err;}
-			list.forEach(function(file) {
-				var module = appFolder +"/node_modules" + '/' + file;
-				fs.stat(module, function(err, stat) {
-					if (stat && stat.isDirectory()) {
-						var exclude = {};
-						//package this module.
-						fs.stat(module+"/package.json", function(err,stat) {
-							if (stat && stat.isFile()) {
-								fs.readFile(module+"/package.json", 'utf8', function (err,data) {
-								  if (err) {
-									return console.log(err);
-								  }
-								  var config = JSON.parse(data);
-								  packModule(module,config.name+"-"+config.version+"-"+process.platform,appFolder);
-								  moduleDependancies[config.name] = {
-									name:config.name
-									,version:config.version
-									,platform:process.platform
-								  }
-								});
-							} else {
-								packModule(module,file+"."+process.platform,appFolder);
-							}
-						});
-						
-					}					
-				});
-			});
-		});
-		
-		
-		var appPackage = appFolder + config.packageExt;
-		process.stdout.write('scanning folder: '+appFolder+'\n');
-		var exclude= {
-			'bin':'bin'
-			,'node_modules':'node_modules'
-		}
-		walk(appFolder,function(err,files) {
-			if (err) {
-				process.stdout.write('Error:' + err);
-			} else {
-				var apack = require("./node-native-zip");
+		appFolder = process.argv[2];
+		appPackage = appFolder + config.packageExt;
+		fs.exists(appFolder+"/node_modules/dependancies.json",function(exists) {
+			if (!exists) {
+				moduleDependancies = {};
+				scanModules();
 				
-				var archive = new apack(appPackage);
-				archive.addFiles(files, function (err) {
-					if (err) {
-						process.stdout.write("error while adding files: "+ err);
-					} else {
-						var buff = archive.toBuffer();
-						fs.writeFile(appPackage, buff, function () {
-							process.stdout.write("wrote "+appPackage+'\n');
-						});
-					}
+			} else {
+				fs.readFile(appFolder+"/node_modules/dependancies.json", 'utf8', function (err,data) {
+				  if (err) {
+					console.log(err);
+				  } else {
+					moduleDependancies = JSON.parse(data);
+				  }
+					scanModules();
 				});
 			}
-		},exclude);
+		});
 	}
 	if (stats.isFile()) {
 		var apack = require('./adm-zip');
@@ -88,8 +58,82 @@ fs.stat(process.argv[2], function(err, stats) {
 		process.stdout.write("unpackaged to "+appFolder+'\n');
 	}
 });
+
+function packageApp() {
+	process.stdout.write('scanning folder: '+appFolder+'\n');
+	var exclude= {
+		'bin':'bin'
+		,'node_modules':'node_modules'
+	}
+	walk(appFolder,function(err,files) {
+		if (err) {
+			process.stdout.write('Error:' + err);
+		} else {
+			var apack = require("./node-native-zip");
+			
+			var archive = new apack(appPackage);
+			archive.addFiles([ 
+				{ name: "dependancies.json", path: appFolder+"/node_modules/dependancies.json" }
+			], function (err) {
+				if (err) console.log("error while adding files: " + err);
+				archive.addFiles(files, function (err) {
+					if (err) {
+						process.stdout.write("error while adding files: "+ err);
+					} else {
+						var buff = archive.toBuffer();
+						fs.writeFile(appPackage, buff, function () {
+							process.stdout.write("wrote "+appPackage+'\n');
+						});
+					}
+				});
+			});
+		}
+	},exclude);
+}
+
+
+function scanModules() {
+//scan node modules
+	console.log("scanning node_modules");
+	fs.readdir("./"+appFolder+"/node_modules", function(err, list) {
+		if (err) {console.log(err);throw err;}
+		list.forEach(function(file) {
+			var module = appFolder +"/node_modules" + '/' + file;
+			fs.stat(module, function(err, stat) {
+				if (stat && stat.isDirectory()) {
+					var exclude = {};
+					//package this module.
+					fs.stat(module+"/package.json", function(err,stat) {
+						if (stat && stat.isFile()) {
+							fs.readFile(module+"/package.json", 'utf8', function (err,data) {
+							  if (err) {
+								return console.log(err);
+							  }
+							  var config = JSON.parse(data);
+							  if (!moduleDependancies[config.name]) moduleDependancies[config.name] = {};
+							  
+							  moduleDependancies[config.name].name = config.name;
+							  moduleDependancies[config.name].version = config.version;
+							  if (!moduleDependancies[config.name]['platforms']) moduleDependancies[config.name].platforms = {};
+							  moduleDependancies[config.name].platforms[process.platform] = process.platform;
+							  
+							  
+							  packModule(module,config.name+"-"+config.version+"-"+process.platform,appFolder);
+							  
+							});
+						} else {
+							packModule(module,file+"."+process.platform,appFolder);
+						}
+					});
+					
+				}					
+			});
+		});
+	});
+}
 //package module
 function packModule(module,moduleName,appFolder) {
+	modulesWaiting++;
 	var modulePack = appFolder+"/node_modules/"+moduleName+".modpack";
 	var exclude = {};
 	walk(module,function(err,files) {
@@ -105,6 +149,9 @@ function packModule(module,moduleName,appFolder) {
 					var buff = archive.toBuffer();
 					fs.writeFile(modulePack, buff, function () {
 						process.stdout.write("wrote "+modulePack+'\n');
+						
+						if (!--modulesWaiting) {modulesWritten();}
+	
 					});
 				}
 			});
